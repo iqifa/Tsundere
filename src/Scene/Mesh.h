8d773a958701e1480681c5bcc90c23d54f0011be
1264317588 <1264317588@qq.com>
2023-05-22 21:10:03 +0800

    MeshUpdateTest

--- src/Scene/Mesh.h
+++ src/Scene/Mesh.h
@@ -82,7 +82,8 @@ public:
                 number = std::to_string(heightNr++); // transfer unsigned int to string
 
             // now set the sampler to the correct texture unit
-            glUniform1i(glGetUniformLocation(shader.GetID(), (name + number).c_str()), i);
+            //glUniform1i(glGetUniformLocation(shader.GetID(), (name + number).c_str()), i);
+            shader.SetUniform1i((name + number).c_str(), i);
             // and finally bind the texture
             glBindTexture(GL_TEXTURE_2D, textures[i].id);
         }
