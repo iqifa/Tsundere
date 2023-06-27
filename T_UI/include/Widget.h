ce1df1d8e9fa35811224b2697b7158adca19d610
1264317588 <1264317588@qq.com>
2023-06-28 01:24:42 +0800

    添加自定义组件，通过lambda函数去自定义绘制

--- T_UI/include/Widget.h
+++ T_UI/include/Widget.h
@@ -1,35 +1,38 @@
 #pragma once
-
+#ifndef WIDGET
+#define WIDGET
 
 #include<string>
 #include"Event\Event.h"
 #include"Event\Event.inl"
+#include "../../Core/src/vender/imgui/imgui.h"
+#include"../../Core/src/vender/glm/glm.hpp"
+#include"../../Core/src/vender/glm/gtc/matrix_integer.hpp"
 using namespace std;
-#ifndef WIDGET
-#define WIDGET
+
 
 namespace Widget
 {
-	bool SelectButton(ImTextureID img, ImVec2 size, bool selected)
-	{
-
-		if (selected)
-		{
-			ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(255, 255, 255, 255));
-		}
-		else
-			ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(255, 255, 0, 255));
-		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(255, 255, 0, 255));
-		bool rec = ImGui::ImageButton(img, size, ImVec2{ 0,1 }, ImVec2{ 1,0 });
-
-		ImGui::PopStyleColor(2);
-
-		return rec;
-	}
+	/// <summary>
+	/// override _Draw() 
+	/// </summary>
 	class Widget {
 	public:
 		virtual void _Draw() = 0;
 	};
+	/*template<class...Argstype>*/
+	class DiyWidget :public Widget {
+	public:
+		using Callback = std::function<void()>;
+		DiyWidget(Callback lambda):m_lambda(lambda){}
+
+		virtual void _Draw() override {
+			m_lambda();
+		}
+	protected:
+		Callback m_lambda;
+	};
+
 	class Button:public	Widget {
 	public:
 		Eventing::Event<> ClickEvents;
@@ -60,6 +63,19 @@ namespace Widget
 		ImTextureID img_id;
 		ImVec2 size;
 		bool selected = false;
+	};
+
+	class Separator :public Widget {
+	public:
+		Separator(string Lable="") :Lable(Lable) {}
+		void _Draw() override {
+			ImGui::Columns(1);
+			ImGui::PushStyleVar(ImGuiStyleVar_SeparatorTextAlign, ImVec2{ 0.5f,0.5f });
+			ImGui::SeparatorText(Lable.c_str());
+			ImGui::PopStyleVar();
+		}
+	private:
+		string Lable;
 	};
 
 }
