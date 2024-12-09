f9d569e42955d7d86a88e6aea461973f408bafbf
iaifa <1264317588@qq.com>
2024-12-09 11:08:46 +0800

    Reapply "Notes"
    This reverts commit 3edbb83759bcc155b9928e2b7fa69da911de298a.

--- T_Core/src/Panels/Material.h
+++ T_Core/src/Panels/Material.h
@@ -11,38 +11,7 @@ enum class ValueType {
 	NONE=-1,INT=0,FLOAT=1,DOUBLE=2,CHAR=3,STR=4,VEC3=5,VEC2=6,TEXTURE=7,HEADER=8
 };
 
-//template<>
-//void ValueChange<float>(string name, unsigned int a, Ref<Shader> shader)
-//{
-//	float* value = (float*)a;
-//	shader->SetUniform1i("")
-//}
-//template<>
-//void ValueChange<string>(string name, unsigned int a, Ref<Shader> shader)
-//{
-//	string* value = (string*)a;
-//	char* str = value->data();
-//	ImGui::InputText(name.c_str(), str, 100);
-//	*value = str;
-//}
-//template<>
-//void ValueChange<vec2>(string name, unsigned int a, Ref<Shader> shader)
-//{
-//	vec2* value = (vec2*)a;
-//}
-//template<>
-//void ValueChange<vec3>(string name, unsigned int a, Ref<Shader> shader)
-//{
-//	vec3* value = (vec3*)a;
-//	float* aa = (float*)value;
-//	ImGui::InputFloat3(name.c_str(), aa);
-//}
-//template<>
-//void ValueChange<double>(string name, unsigned int a, Ref<Shader> shader)
-//{
-//	double* value = (double*)a;
-//	ImGui::InputDouble(name.c_str(), value);
-//}
+
 
 class Material
 {
